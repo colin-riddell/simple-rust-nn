43111f5e335aca8cb29a04a3f413e39413ac2e5d
Colin Riddell <colin.riddell@fanduel.com>
2023-09-15 16:19:06 +0100

    refactoring zeros_and_ones to an impl and struct

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ mod utils;
 mod zeros_and_ones;
 mod simple_problem;
 
-use zeros_and_ones::train_on_zeros_and_ones;
-use simple_problem::train_simple_problem;
+use zeros_and_ones::TrainableCharacterModel;
+use zeros_and_ones::FullMnist;
+// use simple_problem::train_simple_problem;
 
 fn main() {
     // train_simple_problem();
-    train_on_zeros_and_ones();
+    let mut mnistModel = FullMnist::new();
+    mnistModel.train();
 }
--- src/zeros_and_ones.rs
+++ src/zeros_and_ones.rs
@@ -11,59 +11,114 @@ use crate::images::load_image;
 
 use crate::network::Network::Network;
 
-fn get_mnist_letter_training(letter: &str)-> Vec<String>{
-    let path = format!("./mnist_png/training/{}", letter);
+pub trait TrainableCharacterModel {
+    fn train(&mut self);
+    fn train_letter(&mut self, letter: &str);
+    // fn test_all();
+    // fn test_one(letter: &str);
+    // fn test_single(path: &str);
+}
+
+// #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FullMnist {
+    network: Network
+}
+impl FullMnist {
+    pub fn new() -> FullMnist {
+        let network = Network::new(784, 10, 36);
+        FullMnist { network }
+    }  
+}
+  
+
+
+impl TrainableCharacterModel for FullMnist {
+
+
+    fn train_letter(&mut self, letter: &str) {
+        // get all files for 1's
+        let one_files = get_mnist_letter_training(letter);
+
+        if let Some(output) = encode_letter(letter) {
+            println!("generating for {} output ", output.len());
+            one_files.iter()
+                .enumerate()
+                .for_each(|(index, file)| {
+                    let inputs = load_image(file);
+                    self.network.train(&inputs, &vec![output.to_vec()], 0.1, 50);
+                });
+        }
+
+
+
+    }
+
+    fn train(&mut self) {
+        let mut initial_weights: Vec<f64> = vec![];
+        for _ in 0..784 {
+            initial_weights.push(random_f64()); // Use a random value instead of 0
+        }
+
+
+        // let one_output = vec![0.0, 1.0];
+        self.train_letter("1");
+
+        // let zero_output = vec![1.0, 0.0];
+        self.train_letter("0");
+
+        // save trained neural network to JSON
+        let mut file = File::create("./network_after_training.json").unwrap();
+        file.write_all(self.network.to_string().as_bytes()).unwrap();
+
+        
+
+
+        // let one_test_files = get_files("./mnist_png/testing/1");
+
+    }
+
+    // fn test_all_letter(&self, letter: &str){
+
+    //         // load file from  test set
+    //         let zero_test_files = get_mnist_letter_testing("0");
+
+    //         for (index, file) in zero_test_files.iter().enumerate() {
+    //             println!("Predicting file {}", file);
+    //             let pixels = load_image(&file);
+        
+    //             let flattened = flatten(&pixels);
+        
+    //             println!("{:?}",self.network.predict(&flattened));
+    //         }
+
+    // }
+
+}
+
+fn encode_letter(c: &str) -> Option<Vec<f64>> {
+    // Define the full range of characters from '0' to 'z'
+    let full_range = "0123456789abcdefghijklmnopqrstuvwxyz";
+    
+    // Initialize a vector with zeros
+    let mut vec = vec![0.0; full_range.len()];
+    
+    // Find the index of the character
+    if let Some(index) = full_range.find(c) {
+        // Set the corresponding index to 1.0
+        vec[index] = 1.0;
+        Some(vec)
+    } else {
+        // Return None if the character is not in the range
+        None
+    }
+}
+
+fn get_mnist_letter_testing(letter: &str)-> Vec<String>{
+    let path = format!("./mnist_png/testing/{}", letter);
     return get_files(&path)
 }
 
-fn train_letter(network: &mut Network, letter: &str, output: &Vec<f64>) {
-    // get all files for 1's
-    let one_files = get_mnist_letter_training(letter);
-
-    one_files.iter()
-        .enumerate()
-        .for_each(|(index, file)| {
-            let inputs = load_image(file);
-            network.train(&inputs, &vec![output.to_vec()], 0.1, 50);
-        });
-
-}
-
-pub fn train_on_zeros_and_ones() {
-    let mut initial_weights: Vec<f64> = vec![];
-    for _ in 0..784 {
-        initial_weights.push(random_f64()); // Use a random value instead of 0
-    }
-
-    let mut network = Network::new(784, 10, 2);
-
-    let one_output = vec![0.0, 1.0];
-    train_letter(&mut network, "1", &one_output);
-
-    let zero_output = vec![1.0, 0.0];
-    train_letter(&mut network, "0", &zero_output);
-
-    // save trained neural network to JSON
-    let mut file = File::create("./network_after_training.json").unwrap();
-    file.write_all(network.to_string().as_bytes()).unwrap();
-
-    
-    // load file from  test set
-    let zero_test_files = get_files("./mnist_png/testing/0");
-
-    for (index, file) in zero_test_files.iter().enumerate() {
-        println!("Predicting file {}", file);
-        let pixels = load_image(&file);
-
-        let flattened = flatten(&pixels);
-
-        println!("{:?}",network.predict(&flattened));
-    }
-
-    let one_test_files = get_files("./mnist_png/testing/1");
-
-}
-
-pub fn test_all(){
-
+fn get_mnist_letter_training(letter: &str)-> Vec<String>{
+    let path = format!("./mnist_png/training/{}", letter);
+    return get_files(&path)
 }
